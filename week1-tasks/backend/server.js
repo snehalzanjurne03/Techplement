5148c664940ccfba6f1865acf8682a235bdd3cff
snehalzanjurne03 <149039095+snehalzanjurne03@users.noreply.github.com>
2024-10-08 16:58:06 +0530

    Update server.js

--- week1-tasks/backend/server.js
+++ week1-tasks/backend/server.js
@@ -5,16 +5,16 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Middleware
+
 app.use(express.json());
 app.use(express.static('frontend'));
 
-// Serve index.html for the root path
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'index.html')); // Serve the index.html file
+    res.sendFile(path.join(__dirname, 'frontend', 'index.html')); 
 });
 
-// Load quotes data asynchronously
+
 let quotes = [];
 fs.readFile(path.join(__dirname, 'quotes.json'), 'utf8', (err, data) => {
     if (err) {
@@ -24,13 +24,13 @@ fs.readFile(path.join(__dirname, 'quotes.json'), 'utf8', (err, data) => {
     quotes = JSON.parse(data);
 });
 
-// API to get a random quote
+
 app.get('/api/quotes/random', (req, res) => {
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
     res.json(randomQuote);
 });
 
-// API to search for quotes by author
+
 app.get('/api/quotes', (req, res) => {
     const author = req.query.author;
     const filteredQuotes = quotes.filter(quote => 
@@ -39,7 +39,7 @@ app.get('/api/quotes', (req, res) => {
     res.json(filteredQuotes);
 });
 
-// Start server
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
